9af4268e779e68db0b3a595e835a3cb965089c38
roalves1234 <roalves1234@hotmail.com>
2025-09-27 19:15:03 -0300

    Flask

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Olá VPS...")
+from flask import Flask
+
+# Cria a aplicação Flask
+app = Flask(__name__)
+
+# Define a rota principal ("/")
+@app.route("/")
+def home():
+    return "<h1>Olá VPS!!</h1>"
+
+# Inicia o servidor
+if __name__ == "__main__":
+    app.run(debug=True)
